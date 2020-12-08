05945283f7378a0970d7111405da834ebd089ea1
wugang <996528491@qq.com>
2020-12-08 11:00:32 +0800

    两数相加

---
+++ src/addTwoNumbers/ListNode.java
@@ -0,0 +1,17 @@
+package addTwoNumbers;
+
+/**
+ * @author wugang
+ * @date 2020/12/1 14:21
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
---
+++ src/addTwoNumbers/Test.java
@@ -0,0 +1,30 @@
+package addTwoNumbers;
+
+/**
+ * @author wugang
+ * @date 2020/12/1 16:30
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = solution.addTwoNumbers(l1,l2);
+
+        while (result != null){
+            System.out.print(result.val);
+            if (result.next != null){
+                System.out.print("->");
+            }
+
+            result = result.next;
+        }
+    }
+}
